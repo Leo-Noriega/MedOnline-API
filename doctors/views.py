9079d23b0acc1e37794f66de00b2dd896eb62d3c
VulmaroMartinez <20223tn069@utez.edu.mx>
2025-03-17 00:23:17 -0600

    feat: add serializers and viewsets in models

--- doctors/views.py
+++ doctors/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.renderers import JSONRenderer
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
 
-# Create your views here.
+class DoctorViewSet(viewsets.ModelViewSet):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+    renderer_classes = [JSONRenderer]
+    http_method_names = ['get', 'post', 'put', 'delete']
+    
+class SpecialtyViewSet(viewsets.ModelViewSet):
+    queryset = Specialty.objects.all()
+    serializer_class = SpecialtySerializer
+    renderer_classes = [JSONRenderer]
+    http_method_names = ['get', 'post', 'put', 'delete']
+    
+class DoctorSpecialtyViewSet(viewsets.ModelViewSet):
+    queryset = DoctorSpecialty.objects.all()
+    serializer_class = DoctorSpecialtySerializer
+    renderer_classes = [JSONRenderer]
+    http_method_names = ['get', 'post', 'put', 'delete']
+
+class AddressViewSet(viewsets.ModelViewSet):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+    renderer_classes = [JSONRenderer]
+    http_method_names = ['get', 'post', 'put', 'delete']
--- schedule/views.py
+++ schedule/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.renderers import JSONRenderer
+from rest_framework import viewsets
+from .models import Availability
+from .serializers import AvailabilitySerializer
 
-# Create your views here.
+class AvailabilityViewSet(viewsets.ModelViewSet):
+    queryset = Availability.objects.all()
+    serializer_class = AvailabilitySerializer
+    renderer_classes = [JSONRenderer]
+    http_method_names = ['get', 'post', 'put', 'delete']
