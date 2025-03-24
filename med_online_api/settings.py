14d80c84259daefb74d2572367046d0660ba415a
Leo-Noriega <85385095+Leo-Noriega@users.noreply.github.com>
2025-03-24 12:03:43 -0600

    fix (settings): Mailer config
    Added the email user to conf for scalibility

--- med_online_api/settings.py
+++ med_online_api/settings.py
@@ -161,6 +161,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 EMAIL_HOST = 'smtp.googlemail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'medonlineapi@gmail.com'
+EMAIL_HOST_USER = conf["email_user"]
 EMAIL_HOST_PASSWORD = conf["email_password"]
 EMAIL_USE_TLS = True
