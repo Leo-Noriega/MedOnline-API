84536520610b8b75d024ee63ab2b9d1aabe9a7cd
Moises <20223tn055@utez.edu.mx>
2025-04-06 22:31:31 -0600

    feat: :zap: admin views and templates
    Added views, templates and ulrs settings for the administrator

--- custom_admin/urls.py
+++ custom_admin/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path,include
-from rest_framework.routers import DefaultRouter
-from .views import AdminUserViewSet, home
-
-router = DefaultRouter()
-router.register(r'users', AdminUserViewSet)
+from .views import *
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('home/', home, name='home'),
+    path('inicio/', home, name='admin_home'),
+    path('gestion-pacientes/', manage_patients, name='manage_patients'),
+    path('gestion-especialistas/', manage_doctors, name='manage_doctors'),
+    path('mi-cuenta/', admin_account, name='admin_account'),
+    path('users/', get_all_users, name='get_all_users'),
+    path('logout/', cerrar_sesion, name='cerrar_sesion'),
+    path('get-admin/<int:user_id>/', get_admin, name='get_admin'),
+    path('update-admin/<int:user_id>/', update_admin, name='update_admin'),
+    path('patients/<int:user_id>/', get_patient, name='get_patient'),
+    path('patients/update/<int:user_id>/', update_patient, name='update_patient'),
+    path('appointments/<int:appointment_id>/status/', update_appointment_status, name='update_appointment_status'),
+    path('doctors/<int:user_id>/', get_doctor, name='get_doctor'),
+    path('doctors/update/<int:user_id>/', update_doctor, name='update_doctor'),
+    path('doctors/<int:doctor_id>/specialties/<int:doctor_specialty_id>/', update_specialty, name='update_specialty'),
+    path('doctors/addresses/<int:address_id>/', update_address, name='update_address'),
+    path('doctors/<int:doctor_id>/schedules/', update_schedule, name='update_schedule'),
 ]
--- med_online_api/urls.py
+++ med_online_api/urls.py
@@ -24,12 +24,11 @@ urlpatterns = [
     path('', index, name='landing'),
     path('registro/', register, name='registro'),
     path('quienes-somos/', about_us, name='quienes-somos'),
-    path('admin/', admin.site.urls),
     path('especialista/', include('doctors.urls')),
     path('schedule/', include('schedule.urls')),
     path('reviews/', include('reviews.urls')),
     path('users/', include('users.urls')),
     path('appointments/', include('appointments.urls')),
     path('email/', include('mails.urls')),
-    path('api/admin/', include('custom_admin.urls')),
+    path('admin/', include('custom_admin.urls')),
 ]
