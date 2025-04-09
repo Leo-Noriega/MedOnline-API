b95b5f5bb846878378a2af1a341d7db108612492
LeoDoCa <20223tn049@utez.edu.mx>
2025-04-08 21:14:26 -0600

    feat: Password reset functionality
    Add password recovery and reset function, with sending of emails, and add token field in user model

--- users/urls.py
+++ users/urls.py
@@ -13,4 +13,8 @@ urlpatterns = [
     path('token-refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('login/', CustomLoginViewAPI.as_view(), name='login'),
     path('logout/', cerrar_sesion, name='cerrar_sesion'),
+    path("send-reset-email/", send_reset_email, name="send_reset_email"),
+    path("reset-password/", reset_password, name="reset_password"),
+    path('recovery-password/', recovery_password_view, name='recovery_password_view'),
+    path('reset-password/<str:token>/', reset_password_view, name='reset_password_view'),
 ]
